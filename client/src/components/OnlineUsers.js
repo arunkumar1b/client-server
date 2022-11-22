db8fa8ab95f5559a5defcbff300b1504260f2378
arunkumar1b <107618243+arunkumar1b@users.noreply.github.com>
2022-11-22 13:53:31 +0530

    Update OnlineUsers.js

--- client/src/components/OnlineUsers.js
+++ client/src/components/OnlineUsers.js
@@ -1,26 +1,46 @@
 import React from "react";
-
 const OnlineUsers = (props) => {
   const { onUserSelect, users, username, checkUnseenMessages } = props;
-  console.log("checkUnseenMessages",users)
+  console.log("checkUnseenMessages", username);
   return (
     <div>
       <div className="online-users-header">
         <div style={{ margin: "0 10px" }}>Online Users</div>
       </div>
-      <ul className="users-list">
+      <ul className="users-list col-sm-4">
         {users &&
           Object.keys(users).map((user, i) => (
             <>
               {user !== username ? (
-                <li key={i} onClick={() => onUserSelect(user)}>
-                  <span style={{ textTransform: "capitalize" }}>{user}</span>
-                  {checkUnseenMessages(user) !== 0 ? (
-                    <span className="new-message-count">
-                      {checkUnseenMessages(user)}
-                    </span>
-                  ) : null}
-                </li>
+                // <li key={i} onClick={() => onUserSelect(user)}>
+                //   <span style={{ textTransform: "capitalize" }}>{user}</span>
+                //   {checkUnseenMessages(user) !== 0 ? (
+                //     <span className="new-message-count">
+                //       {checkUnseenMessages(user)}
+                //     </span>
+                //   ) : null}
+                // </li>
+                <div key={i} onClick={() => onUserSelect(user)} style={{ paddingRight:"2rem" }}>
+                  <div className="" style={{ width:"fit-content" }}>
+                    <div className="item">
+                      {checkUnseenMessages(user) !== 0 ? (
+                        <span className="notify-badge">
+                          {" "}
+                          {checkUnseenMessages(user)}
+                        </span>
+                      ) : null}
+                      <img
+                        src={require(`../assets/avathar.webp`)}
+                        style={{ width:"50px", height:"auto" }}
+                        key={i}
+                        className="avatarImage"
+                      />
+                      </div>
+                    <div key={i} className="no" style={{ fontSize: "18px", textAlign: "center" }}>
+                      {user}
+                    </div>
+                  </div>
+                </div>
               ) : null}
             </>
           ))}
